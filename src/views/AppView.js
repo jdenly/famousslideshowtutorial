6a98c8f82295f23a70243a2b8a3bf5ed2d2892d1
Jon Denly <jonnydee@gmail.com>
2014-05-28 15:29:45 +1000

    Initial commit.

---
+++ src/views/AppView.js
@@ -0,0 +1,89 @@
+/*** AppView ***/
+
+// define this module in Require.JS
+define(function(require, exports, module) {
+
+    // Import additional modules to be used in this view
+    var View = require('famous/core/View');
+    var Surface = require('famous/core/Surface');
+    var Transform = require('famous/core/Transform');
+    var StateModifier = require('famous/modifiers/StateModifier');
+    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var ContainerSurface = require('famous/surfaces/ContainerSurface');
+
+    // import the SlideshowView class
+    var SlideshowView = require('views/SlideshowView');
+
+    function AppView() {
+        View.apply(this, arguments);
+
+        // passing in data
+        var slideshowView = new SlideshowView({
+            data: this.options.data
+        });
+
+        _createCamera.call(this);
+        _createSlideshow.call(this);
+    }
+
+    // Establishes prototype chain for AppView class to inherit from View
+    AppView.prototype = Object.create(View.prototype);
+    AppView.prototype.constructor = AppView;
+
+    AppView.DEFAULT_OPTIONS = {
+        // it's a good idea to add a property in the default options
+        // even when it's undefined
+        data: undefined,
+
+        cameraWidth: 0.5 * window.innerHeight
+    };
+
+    AppView.DEFAULT_OPTIONS.slideWidth = 0.8 * AppView.DEFAULT_OPTIONS.cameraWidth;
+    AppView.DEFAULT_OPTIONS.slideHeight = AppView.DEFAULT_OPTIONS.slideWidth + 40;
+    AppView.DEFAULT_OPTIONS.slidePosition = 0.77 * AppView.DEFAULT_OPTIONS.cameraWidth;
+
+
+    // Define your helper functions and prototype methods here
+    function _createCamera() {
+        var camera = new ImageSurface({
+            size: [this.options.cameraWidth, true],
+            content: 'img/camera.png',
+            properties: {
+                width: '100%'
+            }
+        });
+
+        var cameraModifier = new StateModifier({
+            origin: [0.5, 0],
+            align: [0.5, 0],
+            transform: Transform.behind
+        });
+
+        this.add(cameraModifier).add(camera);
+    }
+
+    function _createSlideshow() {
+        var slideshowView = new SlideshowView({
+            size: [this.options.slideWidth, this.options.slideHeight],
+            data: this.options.data
+        });
+
+        var slideshowModifier = new StateModifier({
+            origin: [0.5, 0],
+            align: [0.5, 0],
+            transform: Transform.translate(0, this.options.slidePosition, 0)
+        });
+
+        var slideshowContainer = new ContainerSurface({
+            properties: {
+                overflow: 'hidden'
+            }
+        });
+
+        this.add(slideshowModifier).add(slideshowContainer);
+        slideshowContainer.add(slideshowView);
+    }
+
+    module.exports = AppView;
+});
+
